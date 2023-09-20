43f7279e96a1130839698f216bf6e708d5ec1a14
TriforceParas <paras.tloz@gmail.com>
2023-09-21 00:50:43 +0530

    completed 12 part 1

--- practice-12/practice-12.js
+++ practice-12/practice-12.js
@@ -8,11 +8,11 @@ add();
 
 // 12(b)
 function runTwice(fun) {
-    fun();
-    fun();
+  fun();
+  fun();
 }
 
-runTwice(function() {
+runTwice(function () {
   console.log('12b');
 });
 
@@ -21,15 +21,67 @@ runTwice(add);
 // 12(c,d)
 function start() {
   document.querySelector('.start-button').innerHTML = 'Loading...';
-  setTimeout(function() {
+  setTimeout(function () {
     document.querySelector('.start-button').innerHTML = 'FInished'
-  },1000)
+  }, 1000)
 }
 
-// 12(e)
+// 12(e,f)
+let timeOutID;
 function addToCart() {
-  document.querySelector('.add-to-cart-button').innerHTML = 'Added';
-  setTimeout(function() {
-    document.querySelector('.add-to-cart-button').innerHTML = 'Add to Cart';
-  },2000)
+  document.querySelector('.status').innerHTML = 'Added';
+  clearTimeout(timeOutID);
+  timeOutID = setTimeout(function () {
+    document.querySelector('.status').innerHTML = '';
+  }, 2000);
 }
+
+// 12(g)
+/*
+let val = 1;
+setInterval(function () {
+  document.querySelector('.title').innerHTML = `(${val++}) New messages`;
+},1000)
+*/
+
+// 12(h)
+let messages = 0;
+function displayTotalMessages(messages) {
+  if (messages <= 0) {
+    document.querySelector('.title').innerHTML = 'Messages';
+  } else if (messages > 0) {
+    document.querySelector('.title').innerHTML = `(${messages}) New messages`;
+  }
+
+}
+function addMessage() {
+  messages++;
+  displayTotalMessages(messages);
+}
+
+function removeMessage() {
+  if (messages <= 0) {
+    messages = 0;
+  } else messages--;
+  displayTotalMessages(messages);
+}
+
+// 12(j,k)
+const multiply = (a, b) => { return a * b; }
+
+console.log(multiply(2, 3));
+console.log(multiply(7, 10));
+
+// 12(l)
+function countPositive(nums) {
+  let count = 0;
+  nums.forEach((num) => {
+    if (num > 0) {
+      count++;
+    }
+  });
+  return count;
+}
+
+console.log(countPositive([1,-3,5]));
+console.log(countPositive([-2,3,-5,7,10]));
